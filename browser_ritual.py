d0429023955e3a87d128116631b8b13fcb449846
Zoe Bachman <zoe.bachman@gmail.com>
2017-03-21 22:12:39 -0400

    first commit

---
+++ browser_ritual.py
@@ -0,0 +1,22 @@
+#selenium webdriver that opens up the browser and goes to email, scrolls, then facebook, scrolls, twitter, scrolls
+
+
+import time, random, ConfigParser
+from selenium import webdriver
+
+
+## Initiate browser
+
+browser = webdriver.Chrome()
+browser.set_window_size(980,1820)
+browser.set_window_position(200,200)
+
+## URLs
+gmail_url = 'http://gmail.com'
+facebook_url ='facebook.com'
+
+browser.get(gmail_url)
+page = BeautifulSoup(browser.page_source, "lxml")
+
+time.sleep(random.uniform(0.5,1.4)) #make this longer?
+browser.get(signinUrl)
